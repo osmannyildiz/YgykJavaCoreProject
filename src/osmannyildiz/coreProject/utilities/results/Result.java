b41e8719c33ea51ed1a80566812df9b1c1c5cc7e
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-01-07 12:08:14 +0300

    Add result classes

---
+++ src/osmannyildiz/coreProject/utilities/results/Result.java
@@ -0,0 +1,25 @@
+package osmannyildiz.coreProject.utilities.results;
+
+public class Result {
+	
+	protected boolean success;
+	protected String message;
+	
+	public Result(boolean success) {
+		this.success = success;
+	}
+	
+	public Result(boolean success, String message) {
+		this(success);
+		this.message = message;
+	}
+	
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+}
